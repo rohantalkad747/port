7370cfb56b175f9dabb0ceadb56c2c0451faaf91
rohantalkad747 <30855683+rohantalkad747@users.noreply.github.com>
2019-04-29 22:13:31 -0400

    add dist files

---
+++ config/index.js
@@ -0,0 +1,27 @@
+// see http://vuejs-templates.github.io/webpack for documentation.
+const path = require('path');
+const prodEnv = require('./prod.env');
+const devEnv = require('./dev.env');
+
+module.exports = {
+  build: {
+    env: prodEnv,
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    productionSourceMap: true,
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    bundleAnalyzerReport: process.env.npm_config_report,
+  },
+  dev: {
+    env: devEnv,
+    port: 8080,
+    autoOpenBrowser: true,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
+    cssSourceMap: false,
+  },
+};
